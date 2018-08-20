8b86ad30241c18efdb4a6377f9d8f4d79e391e3d
Overflowpy <r_penayo@hotmail.com>
2018-08-20 10:48:21 -0400

    Initial Commit!

---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="jumbotron">
+      <h1 class="display-3">Welcome to Community!</h1>
+      <p class="lead">A fully open source forum. Made with love! by @Nubetech.</p>
+      <hr class="my-4">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam suscipit fugiat alias, eos id vero aliquam architecto dolorum voluptas eius. Architecto corporis voluptates aliquam placeat beatae quae, consequatur omnis atque?</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Sing Up</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+};
+</script>
